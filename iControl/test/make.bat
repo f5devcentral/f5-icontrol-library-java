4de6ebf393c9ed0d98e7dea0789aaded430bb383
lunk djedi <lunk.djedi@gmail.com>
2017-01-31 23:19:14 -0500

    Removing 3rd part libraries

--- iControl/test/make.bat
+++
@@ -1,7 +0,0 @@
-Set JAVALIB=C:\dev\Perforce\devcentral\dc4\Labs\iControlAssembly\java\3rdparty\jar\Axis1.fix2
-:Set JAVALIB=D:\src\perforce\DevCentral\DC4\Labs\iControlAssembly\java\3rdparty.fix
-set AXIS_CLASSPATH=%JAVALIB%\axis.jar;%JAVALIB%\commons-discovery.jar;%JAVALIB%\commons-logging.jar;%JAVALIB%\jaxrpc.jar;%JAVALIB%\junit.jar;%JAVALIB%\saaj.jar;%JAVALIB%\wsdl4j.jar;%JAVALIB%\activation.jar;%JAVALIB%\mail.jar
-:c:\jdk1.6.0_20\bin\javac -classpath %AXIS_CLASSPATH%;..\lib\iControl.jar SystemInfo.java
-:c:\jdk1.6.0_20\bin\javac -classpath %AXIS_CLASSPATH%;..\lib\iControl.jar PoolMember.java
-:c:\jdk1.6.0_20\bin\javac -classpath %AXIS_CLASSPATH%;..\lib\iControl.jar Main.java
-c:\Progra~1\Java\jdk1.6.0_45\bin\javac -classpath %AXIS_CLASSPATH%;..\lib\iControl.jar LocalLBMonitor.java
